c7de562a8533b646bea851df5331e5505f4caabe
Sharath H K <158291023+Sharathhk122@users.noreply.github.com>
2025-08-07 20:09:31 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,4 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    strictPort: true,
+    port: 5173,
+    allowedHosts: ['nexauth-sharathhk.onrender.com'], // ✅ Add this line
+  }
 })
